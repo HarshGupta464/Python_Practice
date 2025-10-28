3afd54e2bf9ee85c7e2b8e5c77393627265daf16
Harsh Gupta <harshgupta464@gmail.com>
2025-10-28 17:43:38 +0530

    Added tuples file

---
+++ Tuples practice.py
@@ -0,0 +1,38 @@
+a= ("Oneplus", "Vivo", "Redmi", "Samsung", "Apple")
+print(a)
+# print(a[1:3])
+# print(a[:3])
+# print(a[2:])
+# print(a[::2])
+# print(a[1::2])
+# print(a[::-1])
+# print(a[2::-1])
+
+# for i in a:
+#     print(i)
+#
+# # along with range and length in loop
+#
+# for i in range(len(a)):
+#     print(a[i])
+
+# print("next loop\n")
+#
+# i=0
+# while i <len(a):
+#     print(a[i])
+#     i+=1
+
+# conversion of tuple
+# b=list(a)
+# print(type(b))
+# print(b)
+#
+# b.append("Nokia")
+# print(b)
+# b=tuple(b)
+# print(type(b))
+# print(b)
+
+# print(a.index("Redmi"))
+
