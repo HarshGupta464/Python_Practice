04902077874f39d12e4afd621a3629ad709e228f
Harsh Gupta <harshgupta464@gmail.com>
2025-10-28 17:48:17 +0530

    corrected tuples file

--- Tuples practice.py
+++ Tuples practice.py
@@ -1,38 +1,38 @@
 a= ("Oneplus", "Vivo", "Redmi", "Samsung", "Apple")
 print(a)
-# print(a[1:3])
-# print(a[:3])
-# print(a[2:])
-# print(a[::2])
-# print(a[1::2])
-# print(a[::-1])
-# print(a[2::-1])
+print(a[1:3])
+print(a[:3])
+print(a[2:])
+print(a[::2])
+print(a[1::2])
+print(a[::-1])
+print(a[2::-1])
 
-# for i in a:
-#     print(i)
-#
-# # along with range and length in loop
-#
-# for i in range(len(a)):
-#     print(a[i])
+for i in a:
+    print(i)
 
-# print("next loop\n")
-#
-# i=0
-# while i <len(a):
-#     print(a[i])
-#     i+=1
+print("\n along with range and length in loop\n")
+
+for i in range(len(a)):
+    print(a[i])
+
+print("\n next loop\n")
+
+i=0
+while i <len(a):
+    print(a[i])
+    i+=1
 
 # conversion of tuple
-# b=list(a)
-# print(type(b))
-# print(b)
-#
-# b.append("Nokia")
-# print(b)
-# b=tuple(b)
-# print(type(b))
-# print(b)
+b=list(a)
+print(type(b))
+print(b)
 
-# print(a.index("Redmi"))
+b.append("Nokia")
+print(b)
+b=tuple(b)
+print(type(b))
+print(b)
+
+print(a.index("Redmi"))
 
