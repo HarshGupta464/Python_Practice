373a10b58fbf3191a53a486e5ec8156d2c9fcbc0
Harsh Gupta <harshgupta464@gmail.com>
2025-11-06 15:35:54 +0530

    More practice on dictionaries

--- Dictionaries Practice.py
+++ Dictionaries Practice.py
@@ -44,3 +44,29 @@ Employees={1: {"Name":"John", "Age":23, "Gender":"Male"},
            3:{"Name":"Peter", "Age":22,"Gender":"Male"}}
 print(Employees[2]["Age"])
 print(Employees[1])
+
+print("\nSort a dictionary by values:")
+
+a={"a":12, "b":23, "c":6, "d":91,"e":45}
+a=sorted(a.values())
+print(a)
+
+print("\nDictionary where values are square of keys:")
+
+a={}
+for i in range(1,16):
+    a[i] = i**2
+print(a)
+
+print("\nMultiply all items in a dictionary:")
+a={"a":1, "b":2, "c":3, "d":4, "e":5}
+result=1
+
+for i in a:
+    result *= a[i]
+print(result)
+
+print("\nSort keys:")
+a={12:"a", 56:"b", 23:"c", 48:"d", 5:"c"}
+a=sorted(a.keys())
+print(a)
